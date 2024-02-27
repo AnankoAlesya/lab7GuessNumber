96ed791470988c08f767262ea16500a674f99309
Alesya <ananko_aa_21@mf.grsu.by>
2024-02-27 22:05:27 +0300

    Внедрение технологии Viewbinding

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -11,58 +11,54 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.lab4.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     int comp_num = 0;
-    Button guessBtn;
-    TextView attemptsLeftTxt;
-    TextView hintShowTxt;
-    EditText enterNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println(comp_num);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         comp_num = GuessNum.rndCompNum();
-        guessBtn = (Button) findViewById(R.id.guess_btn);
-        attemptsLeftTxt = (TextView) findViewById(R.id.attempts_left_txt);
-        hintShowTxt = (TextView) findViewById(R.id.hint_show_txt);
-        enterNumber = (EditText) findViewById(R.id.num_user_txt);
         Context context = getApplicationContext();
         View.OnClickListener clckLstnr = new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
                 {
-                    int attemptsLeft = Integer.parseInt(attemptsLeftTxt.getText().toString());
-                    int number = Integer.parseInt(enterNumber.getText().toString());
+                    int attemptsLeft = Integer.parseInt(binding.attemptsLeftTxt.getText().toString());
+                    int number = Integer.parseInt(binding.numUserTxt.getText().toString());
 
                     if (attemptsLeft > 0)
                     {
                         if (number == comp_num)
                         {
-                            guessBtn.setText(R.string.guessed_str);
-                            guessBtn.setClickable(false);
+                            binding.guessBtn.setText(R.string.guessed_str);
+                            binding.guessBtn.setClickable(false);
                             Toast t = Toast.makeText(context, R.string.wishYou, Toast.LENGTH_LONG);
                             t.show();
                         }
                         else if (number < comp_num)
                         {
-                            hintShowTxt.setText(R.string.hint_more);
+                            binding.hintShowTxt.setText(R.string.hint_more);
                         }
                         else if (number > comp_num)
                         {
-                            hintShowTxt.setText(R.string.hint_less);
+                            binding.hintShowTxt.setText(R.string.hint_less);
                         }
 
                         attemptsLeft--;
-                        attemptsLeftTxt.setText(Integer.toString(attemptsLeft));
+                        binding.attemptsLeftTxt.setText(Integer.toString(attemptsLeft));
                     }
 
                     if (attemptsLeft == 0)
                     {
-                        hintShowTxt.setText(R.string.defeat);
+                        binding.hintShowTxt.setText(R.string.defeat);
                         Toast t = Toast.makeText(context, R.string.defeat, Toast.LENGTH_LONG);
                         t.show();
                     }
@@ -70,15 +66,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        guessBtn.setOnClickListener(clckLstnr);
+        binding.guessBtn.setOnClickListener(clckLstnr);
     }
 
     public void restart(View view) {
         comp_num = GuessNum.rndCompNum();
-        guessBtn.setText(R.string.guess_str);
-        attemptsLeftTxt.setText(R.string.attempts_left_str);
-        hintShowTxt.setText(R.string.hint_show_str);
-        guessBtn.setClickable(true);
-        enterNumber.setText("");
+        binding.guessBtn.setText(R.string.guess_str);
+        binding.attemptsLeftTxt.setText(R.string.attempts_left_str);
+        binding.hintShowTxt.setText(R.string.hint_show_str);
+        binding.guessBtn.setClickable(true);
+        binding.numUserTxt.setText("");
     }
 }
