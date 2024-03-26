473ffabf49054c947030a05fa7a3f1e4a4dd1b2a
Kot Alexey <leshakot10@gmail.com>
2024-03-26 16:34:13 +0300

    Рефакторинг кода

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.lab4;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipboardManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -21,18 +16,13 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lab4.databinding.ActivityMainBinding;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     int gameMode = 0;
 
     MenuItem share;
+
     MenuItem save;
 
     Date currentDate;
@@ -181,15 +172,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==R.id.copy_text) {
+        if (item.getItemId() == R.id.copy_text) {
             ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
             ClipData clip = ClipData.newPlainText("label", binding.numUserTxt.getText().toString());
             clipboard.setPrimaryClip(clip);
             String message = getResources().getString(R.string.copy_message);
-            showMessage(getApplicationContext(),message);
+            showMessage(message);
         }
 
-        if (item.getItemId()==R.id.inset_text) {
+        if (item.getItemId() == R.id.insert_text) {
             ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
             if (clipboard.hasPrimaryClip()) {
                 ClipData.Item item_data = clipboard.getPrimaryClip().getItemAt(0);
@@ -227,26 +218,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void restart(View view) {
+        startNewGame(gameMode);
+    }
+
+    private void startNewGame(int gameMode) {
         binding.guessBtn.setText(R.string.guess_str);
-        binding.guessBtn.setClickable(true);
         binding.numUserTxt.setText("");
+
+        binding.guessBtn.setClickable(true);
         share.setEnabled(false);
         save.setEnabled(false);
 
-        if (gameMode == 0)
-        {
+        if (gameMode == 0) {
             attemptsLeft = 5;
             binding.hintShowTxt.setText(R.string.hint_show_str_2);
             comp_num = GuessNum.rndCompNum(10, 99);
-        }
-        else if (gameMode == 1)
-        {
+        } else if (gameMode == 1) {
             attemptsLeft = 7;
             binding.hintShowTxt.setText(R.string.hint_show_str_3);
             comp_num = GuessNum.rndCompNum(100, 999);
-        }
-        else
-        {
+        } else {
             attemptsLeft = 10;
             binding.hintShowTxt.setText(R.string.hint_show_str_4);
             comp_num = GuessNum.rndCompNum(1000, 9999);
@@ -255,10 +246,9 @@ public class MainActivity extends AppCompatActivity {
         binding.attemptsLeftTxt.setText(Integer.toString(attemptsLeft));
     }
 
-    public void showMessage(Context context, String message)
+    public void showMessage(String message)
     {
-        Toast t = Toast.makeText(context, message, Toast.LENGTH_LONG);
-        t.show();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
     private void openWindowChooseGameMode() {
@@ -267,36 +257,8 @@ public class MainActivity extends AppCompatActivity {
 
         builder.setSingleChoiceItems(R.array.diaps_array, gameMode, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                binding.guessBtn.setText(R.string.guess_str);
-                binding.guessBtn.setClickable(true);
-                binding.numUserTxt.setText("");
-                share.setEnabled(false);
-                save.setEnabled(false);
-
-                if (which == 0)
-                {
-                    attemptsLeft = 5;
-                    binding.hintShowTxt.setText(R.string.hint_show_str_2);
-                    comp_num = GuessNum.rndCompNum(10, 99);
-                    gameMode = 0;
-                }
-                else if (which == 1)
-                {
-                    attemptsLeft = 7;
-                    binding.hintShowTxt.setText(R.string.hint_show_str_3);
-                    comp_num = GuessNum.rndCompNum(100, 999);
-                    gameMode = 1;
-                }
-                else
-                {
-                    attemptsLeft = 10;
-                    binding.hintShowTxt.setText(R.string.hint_show_str_4);
-                    comp_num = GuessNum.rndCompNum(1000, 9999);
-                    gameMode = 2;
-                }
-
-                binding.attemptsLeftTxt.setText(Integer.toString(attemptsLeft));
-                binding.numUserTxt.setText("");
+                startNewGame(which);
+                gameMode = which;
                 dialog.dismiss();
             }
         });
@@ -313,31 +275,26 @@ public class MainActivity extends AppCompatActivity {
         int start = binding.numUserTxt.getSelectionStart();
         int end = binding.numUserTxt.getSelectionEnd();
         int length = binding.numUserTxt.getText().length();
-
-        if (end - start == length && length != 0) {
-            binding.numUserTxt.setText("");
-        }
-
         String digit = "";
 
+        if (end - start == length && length != 0) binding.numUserTxt.setText("");
+
         if (view.getId() == R.id.button_0) digit = "0";
-        else if (view.getId() == R.id.button_1) digit = "1";
-        else if (view.getId() == R.id.button_2) digit = "2";
-        else if (view.getId() == R.id.button_3) digit = "3";
-        else if (view.getId() == R.id.button_4) digit = "4";
-        else if (view.getId() == R.id.button_5) digit = "5";
-        else if (view.getId() == R.id.button_6) digit = "6";
-        else if (view.getId() == R.id.button_7) digit = "7";
-        else if (view.getId() == R.id.button_8) digit = "8";
-        else if (view.getId() == R.id.button_9) digit = "9";
-        else digit = "0";
+        if (view.getId() == R.id.button_1) digit = "1";
+        if (view.getId() == R.id.button_2) digit = "2";
+        if (view.getId() == R.id.button_3) digit = "3";
+        if (view.getId() == R.id.button_4) digit = "4";
+        if (view.getId() == R.id.button_5) digit = "5";
+        if (view.getId() == R.id.button_6) digit = "6";
+        if (view.getId() == R.id.button_7) digit = "7";
+        if (view.getId() == R.id.button_8) digit = "8";
+        if (view.getId() == R.id.button_9) digit = "9";
 
         binding.numUserTxt.setText(binding.numUserTxt.getText().toString() + digit);
         binding.numUserTxt.setSelection(binding.numUserTxt.getText().length());
     }
 
     public void guess(View view) {
-        Context context = getApplicationContext();
         String textNumber = binding.numUserTxt.getText().toString();
         if (textNumber.equals("")) return;
 
@@ -349,10 +306,12 @@ public class MainActivity extends AppCompatActivity {
             {
                 binding.guessBtn.setText(R.string.guessed_str);
                 binding.guessBtn.setClickable(false);
+
                 String message = getResources().getString(R.string.wishYou);
-                showMessage(context, message);
+                showMessage(message);
                 share.setEnabled(true);
                 save.setEnabled(true);
+
                 currentDate = new Date();
                 return;
             }
@@ -371,9 +330,10 @@ public class MainActivity extends AppCompatActivity {
             binding.guessBtn.setClickable(false);
             share.setEnabled(true);
             save.setEnabled(true);
+
             currentDate = new Date();
             String message = getResources().getString(R.string.defeat);
-            showMessage(context, message + " " + comp_num);
+            showMessage(message + " " + comp_num);
         }
     }
 }
